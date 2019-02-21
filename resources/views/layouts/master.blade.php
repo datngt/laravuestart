702e542d38994aaa38b740731dd04d7fa500db1b
datnt93 <dat.ngt1902@gmail.com>
2019-02-21 18:33:27 +0700

    Active menu and HTML5 History Mode

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -171,7 +171,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </router-link>
                     </li>
                     <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link active">
+                        <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-cog"></i>
                             <p>
                                 Management
@@ -180,7 +180,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="#" class="nav-link active">
+                                <a href="#" class="nav-link">
                                     <i class="nav-icon fas fa-dot-circle"></i>
                                     <p>Active Page</p>
                                 </a>

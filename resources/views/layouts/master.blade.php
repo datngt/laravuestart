ee43c8286c22a6d8f3f0f6921b06f8833feb0bee
datnt93 <dat.ngt1902@gmail.com>
2019-02-22 17:13:49 +0700

    User modal window created

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -180,16 +180,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="nav-icon fas fa-dot-circle"></i>
-                                    <p>Active Page</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="nav-icon fas fa-dot-circle"></i>
-                                    <p>Inactive Page</p>
-                                </a>
+                                <router-link to="/users" class="nav-link">
+                                    <i class="nav-icon fas fa-users"></i>
+                                    <p>
+                                        Users
+                                    </p>
+                                </router-link>
                             </li>
                         </ul>
                     </li>

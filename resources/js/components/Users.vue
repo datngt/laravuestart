6236f124abbd4166c513e51c65e4df30dc8ab1f0
datnt93 <dat.ngt1902@gmail.com>
2019-12-20 01:36:16 +0700

    Create User using Axios

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -55,13 +55,45 @@
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="modal-body">
-                        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
+                    <form @submit.prevent="createUser">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input v-model="form.name" type="text" name="name" placeholder="Name"
+                                       class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                                <has-error :form="form" field="name"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <input v-model="form.email" type="email" name="email" placeholder="Email"
+                                       class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                                <has-error :form="form" field="email"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <textarea v-model="form.bio" name="bio" placeholder="Short bio for user (Optional)"
+                                       class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }">
+                                </textarea>
+                                <has-error :form="form" field="bio"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <select v-model="form.type" name="type"
+                                       class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                                    <option value="">Select User Role</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">Standard User</option>
+                                    <option value="author">Author</option>
+                                </select>
+                                <has-error :form="form" field="password"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <input v-model="form.password" type="password" name="password" placeholder="Password"
+                                       class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                                <has-error :form="form" field="password"></has-error>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Create User</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -71,6 +103,27 @@
 
 <script>
     export default {
+        data () {
+            return {
+                // Create a new form instance
+                form: new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: '',
+                })
+            }
+        },
+
+        methods: {
+            createUser () {
+                // Submit the form via a POST request
+                this.form.post('/api/user')
+            }
+        },
+
         mounted() {
             console.log('Component mounted.')
         }
